268da43da1b68e1fd2cee0a73a755a08abadbc9b
AdrianLSY <sites.ad.p3@gmail.com>
2022-05-15 14:24:09 +1000

    updated notice bar & added notice to course

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -24,25 +24,36 @@ class CoursesController < ApplicationController
     @course = Course.new(course_params)
 
     if @course.save
-      redirect_to @course, notice: "Course was successfully created."
+      flash[:success] = "true"
+      flash[:message] = "Successfully created a course."
+      redirect_to @course
     else
-      render :new, status: :unprocessable_entity
+      flash[:success] = "false"
+      flash[:message] = "An error occured while creating a course. Please try again."
+      flash[:errors] = @course.errors
+      redirect_to new_course_path
     end
   end
 
   # PATCH/PUT /courses/1
   def update
     if @course.update(course_params)
-      redirect_to @course, notice: "Course was successfully updated."
+      flash[:success] = "true"
+      flash[:message] = "Successfully edited the course."
+      redirect_to @course
     else
-      render :edit, status: :unprocessable_entity
+      flash[:success] = "false"
+      flash[:message] = "An error occured while editing the course. Please try again."
+      flash[:errors] = @course.errors
+      redirect_to edit_course_path
     end
   end
 
   # DELETE /courses/1
   def destroy
+    flash[:message] = "Course has been deleted."
     @course.destroy
-    redirect_to courses_url, notice: "Course was successfully destroyed."
+    redirect_to courses_url
   end
 
   private
